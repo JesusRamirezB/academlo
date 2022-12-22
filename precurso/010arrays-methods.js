917a5d97c21817dda0c9e866f33667eb7095ff86
Jesus <jesus.ramirez.bocanegra@gmail.com>
2022-12-22 09:39:43 -0600

    Update 010arrays-methods.js

--- precurso/010arrays-methods.js
+++ precurso/010arrays-methods.js
@@ -1,1 +1,11 @@
 // Pop - elimina el ultimo elemento de un arreglo
+// Push - Agrega un elemento al final de un arreglo
+
+// Shift - Elimina el primer elementos de un arreglo
+// Unshift - agrega un elemento al inicio de un arreglo
+
+// Splice - modifica el arreglo
+const emails = ['jesus@gmail.com', 'gabi@gmail.com', 'andres@gmail.com']
+console.log(emails)
+emails.splice(1,0,'ramirez@gmail.com', 'bocanegra@gmail.com')
+console.log(emails)
