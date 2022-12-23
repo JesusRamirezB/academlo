275800982c0d65e08dd2ca6522f22ecdcdcb7b63
Jesus <jesus.ramirez.bocanegra@gmail.com>
2022-12-23 11:37:02 -0600

    New

---
+++ precurso/014objects-and-arrays2.js
@@ -0,0 +1,69 @@
+const users = [
+    {
+        name: 'Erik',
+        age: 29,
+        email: 'erik@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/erik' },
+            { name: 'twitter', url: 'twitter/erik' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Georg',
+        age: 33,
+        email: 'georg@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/georg' },
+            { name: 'twitter', url: 'twitter/georg' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Andrea',
+        age: 42,
+        email: 'andrea@hotmail.com',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+    {
+        name: 'Oscar',
+        age: 31,
+        email: 'oscar@academlo.com',
+        social: [
+            { name: 'facebook', url: 'facebook/oscar' },
+            { name: 'twitter', url: 'twiter/oscar' }
+        ],
+        gender: 'Male'
+    },
+    {
+        name: 'Daniela',
+        age: 22,
+        email: 'daniela@uaq.mx',
+        social: [
+            { name: 'facebook', url: 'facebook/andrea' },
+            { name: 'twitter', url: 'twitter/andrea' }
+        ],
+        gender: 'Female'
+    },
+];
+
+// Extraer todos los correos
+const emails = [];
+emails.push(users[0].email);
+emails.push(users[1].email);
+emails.push(users[2].email);
+emails.push(users[3].email);
+emails.push(users[4].email);
+console.log(emails);
+// Extraer todas las cuentas de facebook
+const facebooks = [];
+facebooks.push(users[0].social[0].url);
+facebooks.push(users[1].social[0].url);
+facebooks.push(users[2].social[0].url);
+facebooks.push(users[3].social[0].url);
+facebooks.push(users[4].social[0].url);
+console.log(facebooks);
